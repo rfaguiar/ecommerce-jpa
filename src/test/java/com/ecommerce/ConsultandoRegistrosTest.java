e7cd22d951d1318731a59130a72198af6ddb6f67
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-11-09 19:39:35 -0300

    Criando uma classe genérica para testes

--- src/test/java/com/ecommerce/ConsultandoRegistrosTest.java
+++ src/test/java/com/ecommerce/ConsultandoRegistrosTest.java
@@ -1,43 +1,12 @@
 package com.ecommerce;
 
 import com.ecommerce.model.Produto;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
-public class ConsultandoRegistrosTest {
-
-    private static EntityManagerFactory entityManagerFactory;
-    private EntityManager entityManager;
-
-    @BeforeClass
-    public static void setUpBeforeClass() {
-        entityManagerFactory = Persistence.createEntityManagerFactory("Ecommerce-PU");
-    }
-
-    @AfterClass
-    public static void tearDownAfterClass() {
-        entityManagerFactory.close();
-    }
-
-    @Before
-    public void setUp() {
-        entityManager = entityManagerFactory.createEntityManager();
-    }
-
-    @After
-    public void tearDown() {
-        entityManager.close();
-    }
+public class ConsultandoRegistrosTest extends EntityManagerTest {
 
     @Test
     public void buscarPorIdentificador() {
