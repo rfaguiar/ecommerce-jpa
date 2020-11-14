5db0e92be98b6bd519a8394f92108123df4cc878
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-11-14 16:04:35 -0300

    executando roolback

---
+++ src/test/java/com/ecommerce/conhecendoentitymanager/GerenciamentoTransacoesTest.java
@@ -0,0 +1,30 @@
+package com.ecommerce.conhecendoentitymanager;
+
+import com.ecommerce.EntityManagerTest;
+import com.ecommerce.model.Pedido;
+import com.ecommerce.model.StatusPedido;
+import org.junit.Test;
+
+public class GerenciamentoTransacoesTest extends EntityManagerTest {
+
+    @Test(expected = Exception.class)
+    public void abrirFecharCancelartransacao() {
+        try {
+            entityManager.getTransaction().begin();
+            metodoDeNegocio();
+            entityManager.getTransaction().commit();
+        } catch (Exception e) {
+            entityManager.getTransaction().rollback();
+            throw e;
+        }
+    }
+
+    private void metodoDeNegocio() {
+        var pedido = entityManager.find(Pedido.class, 1);
+        pedido.setStatus(StatusPedido.PAGO);
+
+        if (pedido.getPagamento() == null) {
+            throw new RuntimeException("Pedido ainda não foi pago.");
+        }
+    }
+}
