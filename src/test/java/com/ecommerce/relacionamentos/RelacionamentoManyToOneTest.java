b0da1a7ee8d1e5f10fef2701a5492c9f9a4bf97d
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-11-15 17:17:05 -0300

    usando @IdClass

--- src/test/java/com/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
+++ src/test/java/com/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
@@ -53,23 +53,40 @@ public class RelacionamentoManyToOneTest extends EntityManagerTest {
 
     @Test
     public void verificarRelacionamentoItemPedidoProduto() {
-        var produto = entityManager.find(Produto.class, 1);
-        var pedido = entityManager.find(Pedido.class, 1);
-        var itemPedido = ItemPedido.builder()
-                .precoProduto(BigDecimal.ONE)
-                .quantidade(5)
-                .produtoId(produto.getId())
-                .pedidoId(pedido.getId())
-                .pedido(pedido)
-                .produto(produto)
-                .build();
+        entityManager.getTransaction().begin();
+
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        Pedido pedido = new Pedido();
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+        pedido.setDataCriacao(LocalDateTime.now());
+        pedido.setTotal(BigDecimal.TEN);
+        pedido.setCliente(cliente);
+
+        entityManager.persist(pedido);
+
+        // Pode ser que logo ao executar o método "persist" o JPA já faça a sincronização com a base.
+        // Mas caso isso não aconteça, o flush garante a sincronização.
+        entityManager.flush();
+
+        ItemPedido itemPedido = new ItemPedido();
+        itemPedido.setPedidoId(pedido.getId());
+        itemPedido.setProdutoId(produto.getId());
+        itemPedido.setPrecoProduto(produto.getPreco());
+        itemPedido.setQuantidade(1);
+        itemPedido.setPedido(pedido);
+        itemPedido.setProduto(produto);
 
         entityManager.persist(itemPedido);
-        entityManager.getTransaction().begin();
+
         entityManager.getTransaction().commit();
+
         entityManager.clear();
 
-        var itemPedidoVerificacao = entityManager.find(ItemPedido.class, new ItemPedidoId(pedido.getId(), produto.getId()));
+        ItemPedido itemPedidoVerificacao = entityManager.find(
+                ItemPedido.class, new ItemPedidoId(pedido.getId(), produto.getId()));
+        assertNotNull(itemPedidoVerificacao.getPedido());
         assertNotNull(itemPedidoVerificacao.getProduto());
     }
 }
