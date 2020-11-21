7a209908db2c1454dc652bea48d3013187775196
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-11-21 20:48:23 -0300

    fazendo atualizações em cascata

--- src/test/java/com/ecommerce/operacoesemcascata/CascadeTypeMergeTest.java
+++ src/test/java/com/ecommerce/operacoesemcascata/CascadeTypeMergeTest.java
@@ -7,22 +7,19 @@ import com.ecommerce.model.ItemPedido;
 import com.ecommerce.model.ItemPedidoId;
 import com.ecommerce.model.Pedido;
 import com.ecommerce.model.Produto;
-import com.ecommerce.model.SexoCliente;
 import com.ecommerce.model.StatusPedido;
 import org.junit.Test;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
 import java.util.Set;
 
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-public class CascadeTypePersistTest extends EntityManagerTest {
+public class CascadeTypeMergeTest extends EntityManagerTest {
 
-//    @Test
-    public void persistirPedidoComItens() {
+    //    @Test
+    public void atualizarPedidoComItens() {
         var cliente = entityManager.find(Cliente.class, 1);
         var produto = entityManager.find(Produto.class, 1);
 
@@ -31,96 +28,89 @@ public class CascadeTypePersistTest extends EntityManagerTest {
                 .total(produto.getPreco())
                 .status(StatusPedido.AGUARDANDO)
                 .build();
+        pedido.setId(1);
 
         var itemPedido = ItemPedido.builder()
                 .id(new ItemPedidoId())
                 .pedido(pedido)
                 .produto(produto)
-                .quantidade(1)
+                .quantidade(3)
                 .precoProduto(produto.getPreco())
                 .build();
-        pedido.setItensPedido(Set.of(itemPedido)); //CascadeType.PERSIST
+        itemPedido.getId().setPedidoId(pedido.getId());
+        itemPedido.getId().setProdutoId(produto.getId());
+
+        pedido.setItensPedido(Set.of(itemPedido));// CasdadeType.MERGE
 
         entityManager.getTransaction().begin();
-        entityManager.persist(pedido);
+        entityManager.merge(pedido);
         entityManager.getTransaction().commit();
         entityManager.clear();
 
         var pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
         assertNotNull(pedidoVerificacao);
-        assertFalse(pedidoVerificacao.getItensPedido().isEmpty());
+        assertTrue(pedidoVerificacao.getItensPedido()
+                .stream()
+                .findFirst()
+                .get()
+                .getQuantidade()
+                .equals(3));
     }
 
-    @Test
-    public void persistirItemPedidoComPedido() {
+    //    @Test
+    public void atualizarItemPedidoComPedido() {
         var cliente = entityManager.find(Cliente.class, 1);
         var produto = entityManager.find(Produto.class, 1);
 
         var pedido = Pedido.builder()
                 .cliente(cliente)
                 .total(produto.getPreco())
-                .status(StatusPedido.AGUARDANDO)
+                .status(StatusPedido.PAGO)
                 .build();
+        pedido.setId(1);
 
         var itemPedido = ItemPedido.builder()
                 .id(new ItemPedidoId())
                 .pedido(pedido)
                 .produto(produto)
-                .quantidade(1)
+                .quantidade(5)
                 .precoProduto(produto.getPreco())
                 .build();
+        itemPedido.getId().setPedidoId(pedido.getId());
+        itemPedido.getId().setProdutoId(produto.getId());
+
+        pedido.setItensPedido(Set.of(itemPedido));// CasdadeType.MERGE
 
         entityManager.getTransaction().begin();
-        entityManager.persist(itemPedido);
+        entityManager.merge(itemPedido);
         entityManager.getTransaction().commit();
         entityManager.clear();
 
-        var pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
-        assertNotNull(pedidoVerificacao);
-        assertFalse(pedidoVerificacao.getItensPedido().isEmpty());
+        var itemPedidoVerificacao = entityManager.find(ItemPedido.class, itemPedido.getId());
+        assertNotNull(itemPedidoVerificacao);
+        assertTrue(StatusPedido.PAGO.equals(itemPedidoVerificacao.getPedido().getStatus()));
     }
+
 
     @Test
-    public void persistirPedidoComCliente() {
-        var cliente = Cliente.builder()
-                .dataNascimento(LocalDate.of(1980, 1, 1))
-                .sexo(SexoCliente.MASCULINO)
-                .nome("José Carlos")
-                .cpf("0123456789")
-                .build();
-
-        var pedido = Pedido.builder()
-                .cliente(cliente)
-                .total(BigDecimal.ZERO)
-                .status(StatusPedido.AGUARDANDO)
-                .build();
-
-
-        entityManager.getTransaction().begin();
-        entityManager.persist(cliente); //cascadeType.PERSIST
-        entityManager.getTransaction().commit();
-        entityManager.clear();
-
-        var clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
-        assertNotNull(clienteVerificacao);
-    }
-
-//    @Test
-    public void persistirProdutoComCategoria() {
+    public void atualizarProdutoComCategoria() {
         var produto = Produto.builder()
-                .preco(BigDecimal.TEN)
-                .nome("Fones de Ouvido")
-                .descricao("A melhor qualidade de som")
+                .preco(new BigDecimal(500))
+                .nome("Kindle")
+                .descricao("Agora com iluminação embutida ajustável")
                 .build();
+        produto.setId(1);
 
         var categoria = Categoria.builder()
-                .nome("Áudio")
+                .nome("Tablets")
                 .build();
-        produto.setCategorias(Set.of(categoria));//cascadeType.PERSIST
+        categoria.setId(2);
+
+        produto.setCategorias(Set.of(categoria));//cascadeType.MERGE
 
 
         entityManager.getTransaction().begin();
-        entityManager.persist(produto);
+        entityManager.merge(produto);
         entityManager.getTransaction().commit();
         entityManager.clear();
 
@@ -128,5 +118,4 @@ public class CascadeTypePersistTest extends EntityManagerTest {
         var categoriaVerificacao = entityManager.find(Categoria.class, categoria.getId());
         assertNotNull(categoriaVerificacao);
     }
-
 }
