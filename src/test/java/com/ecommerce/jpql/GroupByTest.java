73bc70bbd3d92f3da9b47e1747f504f0f0c21ad8
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-11-24 20:50:39 -0300

    Usando o having para condicionar o agrupamento

--- src/test/java/com/ecommerce/jpql/GroupByTest.java
+++ src/test/java/com/ecommerce/jpql/GroupByTest.java
@@ -37,4 +37,50 @@ public class GroupByTest extends EntityManagerTest {
         lista.forEach(arr -> log.info(arr[0] + ", " + arr[1]));
     }
 
+    @Test
+    public void condicionarAgrupamentoComHaving() {
+//         Total de vendas dentre as categorias que mais vendem.
+        String jpql = "select cat.nome, sum(ip.precoProduto) from ItemPedido ip " +
+                " join ip.produto pro join pro.categorias cat " +
+                " group by cat.id " +
+                " having avg(ip.precoProduto) > 1500 ";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+
+        assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
+
+    @Test
+    public void agruparEFiltrarResultado() {
+//         Total de vendas por mês.
+//        String jpql = "select concat(year(p.dataCriacao), '/', function('monthname', p.dataCriacao)), sum(p.total) " +
+//                " from Pedido p " +
+//                " where year(p.dataCriacao) = year(current_date) " +
+//                " group by year(p.dataCriacao), month(p.dataCriacao) ";
+
+//         Total de vendas por categoria.
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//                " join ip.produto pro join pro.categorias c join ip.pedido p " +
+//                " where year(p.dataCriacao) = year(current_date) and month(p.dataCriacao) = month(current_date) " +
+//                " group by c.id";
+
+//        Total de vendas por cliente
+        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+                " join ip.pedido p join p.cliente c join ip.pedido p " +
+                " where year(p.dataCriacao) = year(current_date) and month(p.dataCriacao) >= (month(current_date) - 3) " +
+                " group by c.id";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+
+        assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
+    }
+
 }
