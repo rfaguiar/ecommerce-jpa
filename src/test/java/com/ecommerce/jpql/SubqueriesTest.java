dde298b0e9ca392769068449a2131e6c9ef33095
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-11-25 21:54:32 -0300

    Criando subqueries com any

--- src/test/java/com/ecommerce/jpql/SubqueriesTest.java
+++ src/test/java/com/ecommerce/jpql/SubqueriesTest.java
@@ -117,8 +117,28 @@ public class SubqueriesTest extends EntityManagerTest {
                 " p.preco > ALL (select precoProduto from ItemPedido where produto = p)";
 
         // Todos os produtos que sempre foram vendidos pelo preco atual.
-//        String jpql = "select p from Produto p where " +
+//        var jpql = "select p from Produto p where " +
 //                " p.preco = ALL (select precoProduto from ItemPedido where produto = p)";
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(jpql, Produto.class);
+
+        List<Produto> lista = typedQuery.getResultList();
+        assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> log.info("ID: " + obj.getId()));
+    }
+
+    @Test
+    public void pesquisarComAny() {
+        // Podemos usar o ANY e o SOME.
+
+        // Todos os produtos que já foram vendidos por um preco diferente do atual
+        var jpql = "select p from Produto p " +
+                " where p.preco <> ANY (select precoProduto from ItemPedido where produto = p)";
+
+        // Todos os produtos que já foram vendidos, pelo menos, uma vez pelo preço atual.
+//        var jpql = "select p from Produto p " +
+//                " where p.preco = ANY (select precoProduto from ItemPedido where produto = p)";
 
         TypedQuery<Produto> typedQuery = entityManager.createQuery(jpql, Produto.class);
 
