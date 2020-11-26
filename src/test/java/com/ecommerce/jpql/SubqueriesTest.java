ad320a6eb3b6f11a6563d5d451da66a281c6246e
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-11-25 21:35:37 -0300

    Criando subqueries Criando subqueries com a expressão in Criando subqueries com a expressão exists

---
+++ src/test/java/com/ecommerce/jpql/SubqueriesTest.java
@@ -0,0 +1,72 @@
+package com.ecommerce.jpql;
+
+import com.ecommerce.EntityManagerTest;
+import com.ecommerce.model.Cliente;
+import com.ecommerce.model.Pedido;
+import com.ecommerce.model.Produto;
+import lombok.extern.java.Log;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+
+@Log
+public class SubqueriesTest extends EntityManagerTest {
+
+    @Test
+    public void pesquisarComExists() {
+        String jpql = "select p from Produto p where exists " +
+                " (select 1 from ItemPedido ip2 join ip2.produto p2 where p2 = p)";
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(jpql, Produto.class);
+
+        List<Produto> lista = typedQuery.getResultList();
+        assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> log.info("ID: " + obj.getId()));
+    }
+
+    @Test
+    public void pesquisarComIN() {
+        String jpql = "select p from Pedido p where p.id in " +
+                " (select p2.id from ItemPedido i2 " +
+                "      join i2.pedido p2 join i2.produto pro2 where pro2.preco > 100)";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+
+        List<Pedido> lista = typedQuery.getResultList();
+        assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> log.info("ID: " + obj.getId()));
+    }
+
+    @Test
+    public void pesquisarSubqueries() {
+//         Bons clientes. Versão 2.
+        String jpql = "select c from Cliente c where " +
+                " 500 < (select sum(p.total) from Pedido p where p.cliente = c)";
+
+//         Bons clientes. Versão 1.
+//        String jpql = "select c from Cliente c where " +
+//                " 500 < (select sum(p.total) from c.pedidos p)";
+
+//         Todos os pedidos acima da média de vendas
+//        String jpql = "select p from Pedido p where " +
+//                " p.total > (select avg(total) from Pedido)";
+
+//         O produto ou os produtos mais caros da base.
+//        String jpql = "select p from Produto p where " +
+//                " p.preco = (select max(preco) from Produto)";
+
+        TypedQuery<Cliente> typedQuery = entityManager.createQuery(jpql, Cliente.class);
+
+        List<Cliente> lista = typedQuery.getResultList();
+        assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> log.info("ID: " + obj.getId() + ", Nome: " + obj.getNome()));
+    }
+
+}
