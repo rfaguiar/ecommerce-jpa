1e86b2f12cbf65aa06c064edad5cdf92cb5061dd
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-11-28 19:45:40 -0300

    Selecionando um atributo da entidade como retorno da consulta

--- src/test/java/com/ecommerce/criteria/BasicoCriteriaTest.java
+++ src/test/java/com/ecommerce/criteria/BasicoCriteriaTest.java
@@ -1,6 +1,7 @@
 package com.ecommerce.criteria;
 
 import com.ecommerce.EntityManagerTest;
+import com.ecommerce.model.Cliente;
 import com.ecommerce.model.Pedido;
 import org.junit.Test;
 
@@ -9,6 +10,7 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class BasicoCriteriaTest extends EntityManagerTest {
@@ -26,6 +28,21 @@ public class BasicoCriteriaTest extends EntityManagerTest {
         TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
         Pedido pedido = typedQuery.getSingleResult();
         assertNotNull(pedido);
+    }
+
+    @Test
+    public void selecionarUmAtributoParaRetorno() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Cliente> criteriaQuery = criteriaBuilder.createQuery(Cliente.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root.get("cliente"));
+
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+
+        TypedQuery<Cliente> typedQuery = entityManager.createQuery(criteriaQuery);
+        Cliente cliente = typedQuery.getSingleResult();
+        assertEquals("Fernando Medeiros", cliente.getNome());
     }
 
 }
