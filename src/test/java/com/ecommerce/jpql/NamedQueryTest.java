05d5a3c62489a7cc63ab0f26965f463379b731e5
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-11-27 21:07:11 -0300

    Externalizando queries em um arquivo XML

--- src/test/java/com/ecommerce/jpql/NamedQueryTest.java
+++ src/test/java/com/ecommerce/jpql/NamedQueryTest.java
@@ -1,6 +1,7 @@
 package com.ecommerce.jpql;
 
 import com.ecommerce.EntityManagerTest;
+import com.ecommerce.model.Pedido;
 import com.ecommerce.model.Produto;
 import org.junit.Test;
 
@@ -18,6 +19,36 @@ public class NamedQueryTest extends EntityManagerTest {
         typedQuery.setParameter("categoria", 2);
 
         List<Produto> lista = typedQuery.getResultList();
+
+        assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void executarConsultaArquivoXMLEspecificoProduto() {
+        TypedQuery<Produto> typedQuery = entityManager
+                .createNamedQuery("Produto.todos", Produto.class);
+
+        List<Produto> lista = typedQuery.getResultList();
+
+        assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void executarConsultaArquivoXMLEspecificoPedido() {
+        TypedQuery<Pedido> typedQuery = entityManager
+                .createNamedQuery("Pedido.todos", Pedido.class);
+
+        List<Pedido> lista = typedQuery.getResultList();
+
+        assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void executarConsultaArquivoXML() {
+        TypedQuery<Pedido> typedQuery = entityManager
+                .createNamedQuery("Pedido.listar", Pedido.class);
+
+        List<Pedido> lista = typedQuery.getResultList();
 
         assertFalse(lista.isEmpty());
     }
