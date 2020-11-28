225d9e13591a3e988ec28771d0988c012c6901b1
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-11-28 19:55:42 -0300

    retornando todos os produtos

--- src/test/java/com/ecommerce/criteria/BasicoCriteriaTest.java
+++ src/test/java/com/ecommerce/criteria/BasicoCriteriaTest.java
@@ -3,6 +3,7 @@ package com.ecommerce.criteria;
 import com.ecommerce.EntityManagerTest;
 import com.ecommerce.model.Cliente;
 import com.ecommerce.model.Pedido;
+import com.ecommerce.model.Produto;
 import org.junit.Test;
 
 import javax.persistence.TypedQuery;
@@ -10,7 +11,10 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 
 public class BasicoCriteriaTest extends EntityManagerTest {
@@ -43,6 +47,19 @@ public class BasicoCriteriaTest extends EntityManagerTest {
         TypedQuery<Cliente> typedQuery = entityManager.createQuery(criteriaQuery);
         Cliente cliente = typedQuery.getSingleResult();
         assertEquals("Fernando Medeiros", cliente.getNome());
+    }
+
+    @Test
+    public void selecionaUmAtributoParaRetorno() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+        criteriaQuery.select(root);
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Produto> resultList = typedQuery.getResultList();
+        assertFalse(resultList.isEmpty());
     }
 
 }
