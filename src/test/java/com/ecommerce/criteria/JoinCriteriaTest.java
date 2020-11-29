42a603aec5bc2bd3fd59651635c25128e8547ac0
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-11-28 21:21:34 -0300

    Fazendo inner join entre as entidades

---
+++ src/test/java/com/ecommerce/criteria/JoinCriteriaTest.java
@@ -0,0 +1,33 @@
+package com.ecommerce.criteria;
+
+import com.ecommerce.EntityManagerTest;
+import com.ecommerce.model.Pagamento;
+import com.ecommerce.model.Pedido;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class JoinCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void fazerJoin() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> fromPedido = criteriaQuery.from(Pedido.class);
+        Join<Pedido, Pagamento> joinPagamento = fromPedido.join("pagamento");
+
+        criteriaQuery.select(fromPedido);
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> pedidos = typedQuery.getResultList();
+        assertEquals(4, pedidos.size());
+    }
+
+}
