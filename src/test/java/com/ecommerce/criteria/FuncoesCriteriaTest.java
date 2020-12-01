dd8932b2a3cce2ac6b44d8485f185b7f4a5a6cd0
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-12-01 18:55:39 -0300

    Usando funções para coleções

--- src/test/java/com/ecommerce/criteria/FuncoesCriteriaTest.java
+++ src/test/java/com/ecommerce/criteria/FuncoesCriteriaTest.java
@@ -123,4 +123,28 @@ public class FuncoesCriteriaTest extends EntityManagerTest {
                         + ", sqrt: " + arr[3]));
     }
 
+    @Test
+    public void aplicarFuncaoColecao() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.multiselect(
+                root.get(Pedido_.id),
+                criteriaBuilder.size(root.get(Pedido_.itensPedido))
+        );
+
+        criteriaQuery.where(criteriaBuilder.greaterThan(
+                criteriaBuilder.size(root.get(Pedido_.itensPedido)), 1));
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(
+                arr[0]
+                        + ", size: " + arr[1]));
+    }
+
 }
