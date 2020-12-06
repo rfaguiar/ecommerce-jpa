dddcf6b6b7f7a4cc44fee3b0c625d713d4733f9f
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-12-06 14:46:39 -0300

    Usando a expressão in

--- src/test/java/com/ecommerce/criteria/ExpressoesCondicionaisCriteriaTest.java
+++ src/test/java/com/ecommerce/criteria/ExpressoesCondicionaisCriteriaTest.java
@@ -248,4 +248,45 @@ public class ExpressoesCondicionaisCriteriaTest extends EntityManagerTest {
         lista.forEach(arr -> System.out.println(arr[0] + ", " + arr[1]));
     }
 
+    @Test
+    public void usarExpressaoIN02() {
+        Cliente cliente01 = entityManager.find(Cliente.class, 1);
+
+        Cliente cliente02 = new Cliente();
+        cliente02.setId(2);
+
+        List<Cliente> clientes = List.of(cliente01, cliente02);
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(root.get(Pedido_.cliente).in(clientes));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Pedido> lista = typedQuery.getResultList();
+        assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void usarExpressaoIN01() {
+        List<Integer> ids = List.of(1, 3, 4, 6);
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(root.get(Pedido_.id).in(ids));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Pedido> lista = typedQuery.getResultList();
+        assertFalse(lista.isEmpty());
+    }
+
 }
