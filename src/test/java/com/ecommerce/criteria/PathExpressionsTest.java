0653b1de316e653001d8264136c77cf46325b1a9
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-12-06 13:58:45 -0300

    consultando pedidos com produto específico

--- src/test/java/com/ecommerce/criteria/PathExpressionsTest.java
+++ src/test/java/com/ecommerce/criteria/PathExpressionsTest.java
@@ -2,18 +2,42 @@ package com.ecommerce.criteria;
 
 import com.ecommerce.EntityManagerTest;
 import com.ecommerce.model.Cliente_;
+import com.ecommerce.model.ItemPedido;
+import com.ecommerce.model.ItemPedido_;
 import com.ecommerce.model.Pedido;
 import com.ecommerce.model.Pedido_;
+import com.ecommerce.model.Produto_;
 import org.junit.Test;
 
 import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
 import static org.junit.Assert.assertFalse;
 
 public class PathExpressionsTest extends EntityManagerTest {
+
+    @Test
+    public void buscarPedidosComProdutoDeIDIgual1Exercicio() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, ItemPedido> joinItemPedido = root.join(Pedido_.itensPedido);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(
+                criteriaBuilder.equal(
+                        joinItemPedido.get(ItemPedido_.produto).get(Produto_.id), 1));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+
+        assertFalse(lista.isEmpty());
+    }
 
     @Test
     public void PathExpression() {
