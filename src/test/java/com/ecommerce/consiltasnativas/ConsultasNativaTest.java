1a1ac8a16dd5f09a049df961e42d0cd1e01f4b44
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-12-07 21:16:57 -0300

    Executando SQL e retornando uma entidade

--- src/test/java/com/ecommerce/consiltasnativas/ConsultasNativaTest.java
+++ src/test/java/com/ecommerce/consiltasnativas/ConsultasNativaTest.java
@@ -1,12 +1,23 @@
 package com.ecommerce.consiltasnativas;
 
 import com.ecommerce.EntityManagerTest;
+import com.ecommerce.model.Produto;
 import org.junit.Test;
 
 import javax.persistence.Query;
 import java.util.List;
 
 public class ConsultasNativaTest extends EntityManagerTest {
+
+    @Test
+    public void executarSQLRetornandoEntidade() {
+        var sql = "select * from produto";
+        Query nativeQuery = entityManager.createNativeQuery(sql, Produto.class);
+
+        List<Produto> resultList = nativeQuery.getResultList();
+
+        resultList.forEach(obj -> System.out.printf("Produto => ED: %s, Nome: %s%n", obj.getId(), obj.getNome()));
+    }
 
     @Test
     public void executarSQL() {
@@ -16,7 +27,6 @@ public class ConsultasNativaTest extends EntityManagerTest {
         List<Object[]> resultList = nativeQuery.getResultList();
 
         resultList.forEach(arr -> System.out.printf("Produto => ED: %s, Nome: %s%n", arr[0], arr[1]));
-
     }
 
 }
