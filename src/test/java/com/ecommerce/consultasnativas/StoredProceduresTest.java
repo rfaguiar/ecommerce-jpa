4a5aee6008b6ae1877712bf7193974aa0308326f
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-12-09 20:33:25 -0300

    atualizando registros com procedures

--- src/test/java/com/ecommerce/consultasnativas/StoredProceduresTest.java
+++ src/test/java/com/ecommerce/consultasnativas/StoredProceduresTest.java
@@ -7,12 +7,36 @@ import org.junit.Test;
 import javax.persistence.ParameterMode;
 import javax.persistence.StoredProcedureQuery;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class StoredProceduresTest  extends EntityManagerTest {
+
+    @Test
+    public void atualizarPrecoProdutoExercicio() {
+        StoredProcedureQuery storedProcedureQuery = entityManager
+                .createStoredProcedureQuery("ajustar_preco_produto", Cliente.class);
+
+        storedProcedureQuery.registerStoredProcedureParameter(
+                "produto_id", Integer.class, ParameterMode.IN);
+
+        storedProcedureQuery.registerStoredProcedureParameter(
+                "percentual_ajuste", BigDecimal.class, ParameterMode.IN);
+
+        storedProcedureQuery.registerStoredProcedureParameter(
+                "preco_ajustado", BigDecimal.class, ParameterMode.OUT);
+
+        storedProcedureQuery.setParameter("produto_id", 1);
+        storedProcedureQuery.setParameter("percentual_ajuste", new BigDecimal("0.1"));
+
+        BigDecimal precoAjustado = (BigDecimal) storedProcedureQuery
+                .getOutputParameterValue("preco_ajustado");
+
+        assertEquals(new BigDecimal("878.9"), precoAjustado);
+    }
 
     @Test
     public void receberListaDaProcedure() {
