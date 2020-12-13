3e84416797d3d63008dcfb5321a127dd9d82ebb5
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-12-13 18:27:25 -0300

    Utilizando metamodel com Entity Graph

--- src/test/java/com/ecommerce/detalhesimportantes/EntityGraphTest.java
+++ src/test/java/com/ecommerce/detalhesimportantes/EntityGraphTest.java
@@ -2,7 +2,9 @@ package com.ecommerce.detalhesimportantes;
 
 import com.ecommerce.EntityManagerTest;
 import com.ecommerce.model.Cliente;
+import com.ecommerce.model.Cliente_;
 import com.ecommerce.model.Pedido;
+import com.ecommerce.model.Pedido_;
 import org.junit.Test;
 
 import javax.persistence.EntityGraph;
@@ -38,6 +40,20 @@ public class EntityGraphTest  extends EntityManagerTest {
 
         Subgraph<Cliente> subgraph = entityGraph.addSubgraph("cliente", Cliente.class);
         subgraph.addAttributeNodes("nome", "cpf");
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery("select p from Pedido p", Pedido.class);
+        typedQuery.setHint("javax.persistence.fetchgraph", entityGraph);
+        var lista = typedQuery.getResultList();
+        assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void buscarAtributosEssenciaisDePedidoFetchGraph03() {
+        EntityGraph<Pedido> entityGraph = entityManager.createEntityGraph(Pedido.class);
+        entityGraph.addAttributeNodes(Pedido_.dataCriacao, Pedido_.status, Pedido_.total);
+
+        Subgraph<Cliente> subgraph = entityGraph.addSubgraph("cliente", Cliente.class);
+        subgraph.addAttributeNodes(Cliente_.nome, Cliente_.cpf);
 
         TypedQuery<Pedido> typedQuery = entityManager.createQuery("select p from Pedido p", Pedido.class);
         typedQuery.setHint("javax.persistence.fetchgraph", entityGraph);
