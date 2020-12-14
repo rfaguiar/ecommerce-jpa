1579dcc4e06f872647fbded3a8ea298c295a8273
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-12-14 20:54:26 -0300

    Modos de cache e a anotação @Cacheable

--- src/test/java/com/ecommerce/cache/CacheTest.java
+++ src/test/java/com/ecommerce/cache/CacheTest.java
@@ -42,6 +42,20 @@ public class CacheTest {
     }
 
     @Test
+    public void analisarOpcoesCache() {
+        Cache cache = entityManagerFactory.getCache();
+
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+
+        System.out.println("Buscando a partir da instância 1:");
+        entityManager1
+                .createQuery("select p from Pedido p", Pedido.class)
+                .getResultList();
+
+        assertTrue(cache.contains(Pedido.class, 1));
+    }
+
+    @Test
     public void removerDoCache() {
         Cache cache = entityManagerFactory.getCache();
 
