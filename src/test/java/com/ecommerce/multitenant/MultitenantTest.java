e773d85671e71975cc7e66461f59689fbf24b314
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-12-22 20:07:06 -0300

    Implementando multitenancy com abordagem por máquina

--- src/test/java/com/ecommerce/multitenant/MultitenantTest.java
+++ src/test/java/com/ecommerce/multitenant/MultitenantTest.java
@@ -12,6 +12,21 @@ import static org.junit.Assert.assertEquals;
 public class MultitenantTest extends EntityManagerFactoryTest {
 
     @Test
+    public void usarAbordagemPorMaquina() {
+        EcmCurrentTenantIdentifierResolver.setTenantIdentifier("algaworks_ecommerce");
+        EntityManager entityManager1 = entityManagerFactory.createEntityManager();
+        Produto produto1 = entityManager1.find(Produto.class, 1);
+        assertEquals("Kindle", produto1.getNome());
+        entityManager1.close();
+
+        EcmCurrentTenantIdentifierResolver.setTenantIdentifier("loja_ecommerce");
+        EntityManager entityManager2 = entityManagerFactory.createEntityManager();
+        Produto produto2 = entityManager2.find(Produto.class, 1);
+        assertEquals("Kindle", produto2.getNome());
+        entityManager2.close();
+    }
+
+    @Test
     public void usarAbordagemPorSchema() {
         EcmCurrentTenantIdentifierResolver.setTenantIdentifier("ecommerce_jpa");
         EntityManager entityManager1 = entityManagerFactory.createEntityManager();
